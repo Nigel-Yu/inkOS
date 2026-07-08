5e1d221e930611283492f021dc4c0c123d8618f0
nigel <nigel.sy.yu@gmail.com>
2026-07-08 15:02:44 +0800

    first commit

---
+++ src/OfflineDashboard.ino
@@ -0,0 +1,32 @@
+#include "EPD.h"
+#include "pic.h"
+
+#define POWER_PIN 7
+#define MS_PER_SEC 60000
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200)
+
+  // Display settings
+  pinMode(POWER_PIN, OUTPUT)
+  digitalWrite(POWER_PIN, HIGH)
+  EPD_GPIOInit();
+}
+
+void loop() {
+  //PAINT_Clear(WHITE)
+  EPD_FastMode1Init(); // initialize screen
+  EPD_Display_Clear();
+  EPD_Update();
+  EPD_Clear_R26A6H(); // clear cache
+
+  // draw
+  EPD_DrawLine(240, 0, 240, 270, BLACK);
+  EPD_DrawLine(540, 0, 540, 0, BLACK);
+
+  EPD_PartUpdate(); // partial update
+  EPD_DeepSleep();
+
+  delay(10 * MS_PER_SEC) // runs every 10 mins
+}
